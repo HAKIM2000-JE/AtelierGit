463b7c550c818aa12f1866c1a99d0cd6b49c7ae1
HAKIM2000-JE <abdelhakimjebabra7@gmail.com>
2021-12-01 11:10:58 +0100

    first commit

---
+++ src/main/java/com/AtelierGit/AtelierGit/AtelierGitApplication.java
@@ -0,0 +1,13 @@
+package com.AtelierGit.AtelierGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierGitApplication.class, args);
+	}
+
+}
